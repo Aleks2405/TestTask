d759621610cbf9174cd1ddbd3a7222be6cbcf482
Aleksander <91732138+Aleks2405@users.noreply.github.com>
2022-11-10 11:57:39 +0300

    Созданная библиотека по названием IFigure исполняемый файл Program

---
+++ IFigure.cs
@@ -0,0 +1,59 @@
+﻿namespace ClassLibraryTest
+{
+    public interface IFigure
+    {
+        void AreaOfThe(double[] numbers);
+    }
+    public class Circle : IFigure
+    {
+        public void AreaOfThe(double[] numbers)
+        {
+            double result = 0;
+            for (int i = 0; i < 1; i++)
+            {
+                numbers[i] = double.Parse(Console.ReadLine());
+                result += numbers[i];
+            }
+            Console.WriteLine($"Площадь вашего круга = {3.14 * result * result}");
+        }
+    }
+    public class Square : IFigure
+    {
+        public void AreaOfThe(double[] numbers)
+        {
+            double result = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                numbers[i] = double.Parse(Console.ReadLine());
+                result += numbers[i];
+            }
+            double p = result / 2;
+            double S = p * (p - numbers[0]) * (p - numbers[1]) * (p - numbers[2]);
+            Console.WriteLine($" Плоащадь Вашего треугольника = {Math.Sqrt(S)}");
+  
+            if (Math.Pow(numbers[2],2) == Math.Pow(numbers[0], 2)+ Math.Pow(numbers[1], 2) ||
+                Math.Pow(numbers[0], 2) == Math.Pow(numbers[2], 2) + Math.Pow(numbers[1], 2) ||
+                Math.Pow(numbers[1], 2) == Math.Pow(numbers[0], 2) + Math.Pow(numbers[2], 2))
+            {
+                Console.WriteLine("Треугольник является прямоугольным");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не является прямоугольным");
+            }
+        }
+    }
+        public class Rectangle : IFigure
+        {
+            public void AreaOfThe(double[] numbers)
+            {
+                double result = 1;
+                for (int i = 0; i < 2; i++)
+                {
+                    numbers[i] = double.Parse(Console.ReadLine());
+                    result *= numbers[i];
+                }
+                Console.WriteLine($"площадь квадрата = {result}");
+            }
+        }  
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibraryTest;
+Console.WriteLine($"Выберите цифру фигуры, площадь которой необходимо посчитать \n 1. Круг \n 2. Треугольник  \n 3. Квадрат ");
+var type = Console.ReadLine();
+IFigure a = null;
+double[] arr;
+switch (type.ToLower())
+{
+    case "1": a = new Circle();
+        Console.WriteLine("Введите радиус круга");
+        arr = new double[1];
+        a.AreaOfThe(arr);
+        break;      
+    case "2":  a = new Square();
+        Console.WriteLine(" Введите данные 3 сторон треугольника");
+        arr = new double[3];
+        a.AreaOfThe(arr);
+        break;
+    case "3": a = new Rectangle();
+        arr= new double[2];
+        Console.WriteLine(" Введите 2 стороны квадрата");
+        a.AreaOfThe(arr);
+        break;
+}
+if (a != null)
+{
+    Console.WriteLine($"Вы успешно посчитали данные по своей фигуре");
+}
+else
+{
+    Console.WriteLine($"Вы ввели не существующую фигуру из списка");
+}
+
+
